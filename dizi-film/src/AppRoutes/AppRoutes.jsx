d94b41a6e3f3b9b60276800194f70cad36cbf554
ygmurrsimsek <ygmurrsimsek@icloud.com>
2025-01-16 14:04:57 +0300

    film arama sayasını routes dosyamıza bağladım

--- dizi-film/src/AppRoutes/AppRoutes.jsx
+++ dizi-film/src/AppRoutes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import SearchPage from '../pages/SearchPage';
 function AppRoutes() {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path='/add' element={<SearchPage />} />
             </Routes>
         </div>
     )
--- dizi-film/src/pages/SearchPage.jsx
+++ dizi-film/src/pages/SearchPage.jsx
@@ -1,1 +1,12 @@
 //Arama sonuçları sayfası
+
+
+function SearchPage() {
+    return (
+        <div>
+            selam ben arama sayfasıyım
+        </div>
+    )
+}
+
+export default SearchPage
