37fb2dcb1b96b830f6afe785763f959a2c1a3ade
ygmurrsimsek <ygmurrsimsek@icloud.com>
2025-01-20 21:20:29 +0300

    izlenen fimler sayfası içinde filmlerin görselleri eklendi

--- dizi-film/src/components/Watched.jsx
+++ dizi-film/src/components/Watched.jsx
@@ -6,16 +6,40 @@ function Watched() {
     console.log("To Watched List:", toWatchedList);
 
     return (
-        <div>
-            <ul>
-                {toWatchedList.map((film) => (
-                    <h3 key={film.id}>
-                        {film.title}
-                    </h3>
-                ))}
-            </ul>
+        <div className="movie-page">
+            <div className="container">
+                <div className="header">
+                    <h1 className="heading">İzlenen Filmler</h1>
 
-
+                    <div className="count-pill">
+                        {toWatchedList.length} {toWatchedList.length < 2 ? "Movie" : "Movies"}
+                    </div>
+                </div>
+                {
+                    toWatchedList.length > 0 ? (
+                        <div className="movie-grid">
+                            {
+                                toWatchedList.map((movie) => (
+                                    <div key={movie.id} className="movie-card">
+                                        <div className="overlay"></div>
+                                        {/* movie olark ulaştık dizi içindeki filmlere .Bu dizinin içinde görsele erişmemiz lazım bunu içinde movie.poster_path diyebiliriz şuan objedeyiz. */}
+                                        {movie.poster_path ? (
+                                            <img
+                                                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                                                alt={movie.title}
+                                            />
+                                        ) : (
+                                            <div className="filler-poster"></div>
+                                        )}
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    ) : ((
+                        <p>No movies in the watch list.</p>
+                    ))
+                }
+            </div>
         </div>
     )
 }
